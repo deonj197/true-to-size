a35f5eaa59c337a627736f9883780d3dda48eaf4
Deon <deonj197@gmail.com>
2019-12-06 00:53:52 -0500

    initial commit

---
+++ src/main/java/com/size/truetosize/TrueToSizeApplication.java
@@ -0,0 +1,13 @@
+package com.size.truetosize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueToSizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueToSizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/size/truetosize/TrueToSizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.size.truetosize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueToSizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
